e2ee7a7d019aa61f0cdfa9e3f2b73cdb1b6946ea
Mindaugas Jukna <mindaugas9494@hotmail.com>
2018-11-01 11:33:32 +0100

    fixed untracked files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -237,3 +237,23 @@ function uuidv4() {
   });
   return id;
 }
+
+function passWord() {
+  var testV = 1;
+  var pass1 = prompt("Please Enter Your Password", " ");
+  while (testV < 3) {
+    if (!pass1) history.go(-1);
+    if (pass1.toLowerCase() == "frontend2018") {
+      alert("Welcome!");
+      window.open("protectpage.html");
+      break;
+    }
+    testV += 1;
+    var pass1 = prompt(
+      "Access Denied - Password Incorrect, Please Try Again.",
+      "Password"
+    );
+  }
+  if ((pass1.toLowerCase() != "password") & (testV == 3)) history.go(-1);
+  return " ";
+}
